93af46393a023b1a7eedb4f604c00bd60cd9691e
stressoid <ivan.botanov@gmail.com>
2016-06-28 21:11:12 +0300

    dz3

--- app/connect.php
+++ app/connect.php
@@ -1,5 +1,5 @@
 <?php
-$dbconn = pg_connect("host=localhost dbname=gbphp user=postgres password=postgres")
+$dbconn = pg_connect("host=localhost dbname=gbphp user=stressoid password=stressoid")
 or die('Could not connect: ' . pg_last_error());
 
 require_once 'templator.php';
